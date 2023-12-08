6c0144d55187de5d76316e2950850c16086613b6
layanmoyura <layanmoyura@gmail.com>
2023-12-08 14:19:22 +0530

    side nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,30 @@ import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 
+import{BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import { MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import{MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import{MatButtonModule} from '@angular/material/button'
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SignupComponent,
-    LoginComponent
+    LoginComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
