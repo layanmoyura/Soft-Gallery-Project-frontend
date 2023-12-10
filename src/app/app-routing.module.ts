ab4b1b46b3bf9e73aacc9d2d0da0b80b60af6231
layanmoyura <layanmoyura@gmail.com>
2023-12-10 11:56:43 +0530

    home mod 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   
   {path:'signup',component:SignupComponent},
   {path: 'login' , component:LoginComponent},
+  {path:'home',component:HomeComponent}
 ];
 
 @NgModule({
