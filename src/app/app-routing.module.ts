863af54359d4800a5c5da33fb42f4e3498344090
layanmoyura <layanmoyura@gmail.com>
2023-12-10 14:51:00 +0530

    home mod 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  
+  {path:'',redirectTo:'/home',pathMatch:'full'},
   {path:'signup',component:SignupComponent},
   {path: 'login' , component:LoginComponent},
   {path:'home',component:HomeComponent}
